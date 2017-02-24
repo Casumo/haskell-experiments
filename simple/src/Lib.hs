1d09c88f58478e4ca1353e33ad90941339f9de8e
Maria Camenzuli <maria.camenzuli@casumo.com>
2017-02-24 15:10:13 +0100

    Adding integerToDigits function

--- simple/src/Lib.hs
+++ simple/src/Lib.hs
@@ -10,6 +10,20 @@ someFunc = putStrLn "someFunc"
 valid :: Integer -> Bool
 valid ccNumber = True
 
+integerToDigits :: Integer -> CC
+integerToDigits x
+  | x < 10 = [x]
+  | otherwise = (x `mod` 10):integerToDigits(x `div` 10)
+
+  -- todo: define an inner function so we can reverse the array of digits
+
+--[] :: x `mod` 10
+--
+--integerToDigits(x `div` 10):(x `mod` 10)
+--[]:1:2
+--1:2:[]
+--++
+
 
 --Double the value of every second digit beginning from the right.
 --That is, the last digit is unchanged; the second-to-last digit is doubled;
